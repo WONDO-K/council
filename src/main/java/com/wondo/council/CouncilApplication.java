eedc01e92f3fd49a355fa13d549e65b273a43657
Wondo-K <wondok95@naver.com>
2023-05-29 18:11:17 +0900

    first commit

---
+++ src/main/java/com/wondo/council/CouncilApplication.java
@@ -0,0 +1,13 @@
+package com.wondo.council;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouncilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouncilApplication.class, args);
+	}
+
+}
